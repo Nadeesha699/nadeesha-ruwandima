75082ad92770c5bb5baab1d653a82e12c76ddeac
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-06-05 19:07:16 +0530

    saved

---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById("mobile-menu").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = 0;
+});
+
+document.getElementById("mobile-menu-close").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
+
+document.getElementById("navigation-link").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
+
+document.getElementById("navigation-link-1").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
+
+document.getElementById("navigation-link-2").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
+
+document.getElementById("navigation-link-3").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
+
+document.getElementById("navigation-link-4").addEventListener("click", () => {
+  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+});
