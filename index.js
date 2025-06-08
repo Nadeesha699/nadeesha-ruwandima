42a02e699e1d48046b8ab064988bec21b8cdb10d
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-06-08 21:53:05 +0530

    saved

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ document.getElementById("mobile-menu").addEventListener("click", () => {
 });
 
 document.getElementById("mobile-menu-close").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("navigation-link").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("navigation-link-1").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("navigation-link-2").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("navigation-link-3").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("navigation-link-4").addEventListener("click", () => {
-  document.getElementById("mobile-menu-panel").style.top = "-1000px";
+  document.getElementById("mobile-menu-panel").style.top = "-10000px";
 });
 
 document.getElementById("btn-readmore").addEventListener("click", () => {
@@ -31,7 +31,11 @@ document.getElementById("btn-readmore").addEventListener("click", () => {
   const currunetProp = window
     .getComputedStyle(element)
     .getPropertyValue("display");
-  currunetProp === "flex"
-    ? (document.getElementById("about-p2-1").style.display = "none")
-    : (document.getElementById("about-p2-1").style.display = "flex");
+  if (currunetProp === "flex") {
+    document.getElementById("btn-readmore").innerText = "read more";
+    document.getElementById("about-p2-1").style.display = "none";
+  } else {
+    document.getElementById("btn-readmore").innerText = "read less";
+    document.getElementById("about-p2-1").style.display = "flex";
+  }
 });
